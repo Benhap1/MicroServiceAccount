9b534d95d5458e507bd2450991b135c328ce4356
Bakhtiyar Bobokulov <b.bobokulov@outlook.com>
2024-11-04 01:33:24 +0300

    feat: Consumer 2 modified

--- src/main/java/ru/skillbox/mc_account/config/CommonEventDeserializer.java
+++ src/main/java/ru/skillbox/mc_account/config/CommonEventDeserializer.java
@@ -4,16 +4,33 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.common.serialization.Deserializer;
 import ru.skillbox.common.events.CommonEvent;
 
 import java.io.IOException;
+import java.util.Map;
 
-public class CommonEventDeserializer extends JsonDeserializer<CommonEvent<?>> {
+public class CommonEventDeserializer implements Deserializer<CommonEvent<?>> {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
-    public CommonEvent<?> deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
-        return objectMapper.readValue(p, CommonEvent.class);
+    public void configure(Map<String, ?> configs, boolean isKey) {
+        // Конфигурация, если она нужна
+    }
+
+    @Override
+    public CommonEvent<?> deserialize(String topic, byte[] data) {
+        try {
+            // Десериализация из JSON
+            return objectMapper.readValue(data, CommonEvent.class);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to deserialize CommonEvent", e);
+        }
+    }
+
+    @Override
+    public void close() {
+        // Закрытие, если нужно
     }
 }
